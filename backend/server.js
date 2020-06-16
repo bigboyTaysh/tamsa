0ed7653c12b342a4daa92f0d2e27284714827eed
bigboyTaysh <pvtryk.w@gmail.com>
2020-06-17 00:36:41 +0200

    [change] - for herroku

--- backend/server.js
+++ backend/server.js
@@ -34,20 +34,6 @@ app.use('/users', usersRouter);
 app.use('/events', eventsRouter);
 app.use('/typeOfEvents', typeOfEventsRouter);
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  //
-  app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname = 'client/build/index.html'));
-  })
-}
-//build mode
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
-})
-
 app.listen(port, () => {
     console.log(`Server is running on port : ${port}`);
 });
