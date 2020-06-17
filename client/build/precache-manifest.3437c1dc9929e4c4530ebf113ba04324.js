fbbddcf3d60c05c6e441192ec0d3d9b1defdb459
bigboyTaysh <pvtryk.w@gmail.com>
2020-06-17 02:13:57 +0200

    [change] - for heroku

---
+++ client/build/precache-manifest.3437c1dc9929e4c4530ebf113ba04324.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6c82d174343637b58c8e14c6e1b3d99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94147549860158fbec29",
+    "url": "/static/css/2.5f98dec3.chunk.css"
+  },
+  {
+    "revision": "13c9a0cfd63dad9c57d8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "94147549860158fbec29",
+    "url": "/static/js/2.9b91fa06.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/static/js/2.9b91fa06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13c9a0cfd63dad9c57d8",
+    "url": "/static/js/main.53188eea.chunk.js"
+  },
+  {
+    "revision": "67c3a7af4b98aacf8660",
+    "url": "/static/js/runtime-main.c3892af8.js"
+  }
+]);
