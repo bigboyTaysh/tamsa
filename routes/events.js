d1f6116360464b9a982f72db9e70223a87319abb
bigboyTaysh <pvtryk.w@gmail.com>
2020-06-22 12:01:18 +0200

    [change] - time format

--- routes/events.js
+++ routes/events.js
@@ -33,7 +33,7 @@ router.route("/add").post((req, res) => {
             title: req.body.title,
             description: req.body.description,
             completed: req.body.completed,
-            date: new Date(req.body.date),
+            date: moment(req.body.date).format("YYYY-MM-DDTHH:mm"),
             type: type,
           });
 
